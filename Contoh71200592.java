770b0e36dc81300967117127aa36dc43bbf08d8d
Silviani <silviani.vanesha@ti.ukdw.ac.id>
2022-02-08 15:49:23 +0700

    commit pertama

---
+++ Contoh71200592.java
@@ -0,0 +1,16 @@
+public class Contoh<7100592> {
+    public static voic main(String args[]) {
+        int x=10;
+        int y=25;
+        int z=x+y;
+
+        System.out.println("Hasil penjumlahan"
+                +"dari x+y = " + z);
+        System.out.println("Hasil akar kuadrat"
+                +"dari z = " + Math.sqrt(z));
+        String namadepan = "Silviani"
+        System.out.println("Nama depan saya adalah = " + namadepan);
+        System.out.println("Nama lengkap saya "
+                + "adalah = " + namadepan + "Vanesha"
+    }
+}
